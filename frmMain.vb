106f13197a1e8334098faeb7776b46559996ee4f
Brandon Nortum <ban69@sage.southhills.edu>
2010-09-09 16:04:37 -0400

    Started programming with info from chapter 3 pg 96

--- frmMain.vb
+++ frmMain.vb
@@ -1,3 +1,7 @@
 ﻿Public Class frmMain
 
+    Private Sub btnDisplayAccountSummary_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisplayAccountSummary.Click
+        Dim fmtStr As String = "{0, 7}{1, 8}{2, 8}{3, 9}{4, 7}{5, 9}"
+
+    End Sub
 End Class
