8b1ee4324fee226b045ee11f40ee726bd77b84a9
Brandon Nortum <ban69@sage.southhills.edu>
2010-09-10 16:25:01 -0400

    class work almost done

--- frmMain.vb
+++ frmMain.vb
@@ -1,7 +1,33 @@
 ﻿Public Class frmMain
 
     Private Sub btnDisplayAccountSummary_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisplayAccountSummary.Click
-        Dim fmtStr As String = "{0, 7}{1, 8}{2, 8}{3, 9}{4, 7}{5, 9}"
+        Dim fmtStr As String = "{0, -15}{1, 15}{2, 15}{3, 15}{4, 15}{5, 15}"
+        Dim readerVar As IO.StreamReader = IO.File.OpenText("..\..\data.txt")
+        Dim strAccountNumber As String = readerVar.ReadLine
+        Dim dblPastDueAmount As Double = CDbl(readerVar.ReadLine)
+        Dim dblPayments As Double = CDbl(readerVar.ReadLine)
+        Dim dblPurchases As Double = CDbl(readerVar.ReadLine)
+        Dim dblFinanceCharges As Double = 0
+        Dim dblCurrentAmtDue As Double = 0
+
+        dblFinanceCharges = (dblPastDueAmount - dblPayments) * 0.015
+        dblCurrentAmtDue = (dblPastDueAmount - dblPayments) + dblFinanceCharges
+
+        lstAccountSummary.Items.Add(String.Format(fmtStr, "Account", "Past Due", "", "", "", "Finance", "Current"))
+        lstAccountSummary.Items.Add(String.Format(fmtStr, "Number", "Amount", "Payments", "Purchases", "Charges", "Amt Due"))
+        lstAccountSummary.Items.Add(String.Format(fmtStr, strAccountNumber, FormatCurrency(dblPastDueAmount), FormatCurrency(dblPayments), FormatCurrency(dblPurchases), FormatCurrency(dblFinanceCharges), FormatCurrency(dblCurrentAmtDue)))
+
+        strAccountNumber = readerVar.ReadLine
+        dblPastDueAmount = CDbl(readerVar.ReadLine)
+        dblPayments = CDbl(readerVar.ReadLine)
+        dblPurchases = CDbl(readerVar.ReadLine)
+
+        dblFinanceCharges = 0
+        dblCurrentAmtDue = 0
+        dblFinanceCharges = (dblPastDueAmount - dblPayments) * 0.015
+        dblCurrentAmtDue = (dblPastDueAmount - dblPayments) + dblFinanceCharges
+
+        lstAccountSummary.Items.Add(String.Format(fmtStr, strAccountNumber, FormatCurrency(dblPastDueAmount), FormatCurrency(dblPayments), FormatCurrency(dblPurchases), FormatCurrency(dblFinanceCharges), FormatCurrency(dblCurrentAmtDue)))
 
     End Sub
 End Class
